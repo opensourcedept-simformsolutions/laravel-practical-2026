3feae93897af5fd7821efe74bf3fad5ebb8ba64e
Dhruv Rana <dhruv.rana@simformsolutions.com>
2026-06-15 17:37:08 +0530

    Add Breeze authentication and RBAC

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role_id',
     ];
 
     /**
@@ -33,6 +34,11 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 
     /**
      * Get the attributes that should be cast.
