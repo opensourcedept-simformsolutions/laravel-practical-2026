3d5fa40693f28f4aa12f5ddd8af3535368201bae
Dhruv Rana <dhruv.rana@simformsolutions.com>
2026-06-15 17:37:08 +0530

    Add Breeze authentication and RBAC

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,9 +18,20 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        $this->call([
+            RoleSeeder::class,
+        ]);
+
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('1'),
+            'role_id' => Role::where('name', 'admin')->first()->id,
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,46 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+
+    Route::get('/admin/dashboard', function () {
+        return 'Admin Dashboard';
+    });
+
+});
+
+Route::middleware(['auth', 'role:resident'])->group(function () {
+
+    Route::get('/resident/dashboard', function () {
+        return 'Resident Dashboard';
+    });
+
+});
+
+Route::middleware(['auth', 'role:gatekeeper'])->group(function () {
+
+    Route::get('/gatekeeper/dashboard', function () {
+        return 'Gatekeeper Dashboard';
+    });
+
+});
+
+require __DIR__.'/auth.php';
