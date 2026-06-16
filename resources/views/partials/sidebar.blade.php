9965efaeec025ddc0d89d04c8a7b75984edf2c8a
Rohan Rohit <rohan.rohit@simformsolutions.com>
2026-06-16 17:45:27 +0530

    Sidebar changes

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,36 +1,21 @@
 
 <div class="sidebar-menu">
-    <x-sidebar-link
-        :href="route('admin.dashboard')"
-        :active="request()->is('admin/dashboard')">
-
-<div class="list-group list-group-flush">
-
-    <a href="{{ route('admin.dashboard') }}" class="list-group-item list-group-item-action bg-dark text-white">
-
+    <x-sidebar-link :href="route('admin.dashboard')" :active="request()->is('admin/dashboard')">
         Dashboard
     </x-sidebar-link>
 
-    {{-- ADMIN --}}
     @can('is-admin')
-        <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-
+        <x-sidebar-link href="#" :active="request()->is('/')">
             Users
         </x-sidebar-link>
 
         <x-sidebar-link href="#" :active="request()->is('/')">
             Residents
         </x-sidebar-link>
-
-
-
     @endcan
 
-
-    {{-- GATEKEEPER --}}
     @can('is-gatekeeper')
-        <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-
+        <x-sidebar-link href="#">
             Visitors
         </x-sidebar-link>
 
@@ -38,13 +23,10 @@
             Entry Logs
 
         </x-sidebar-link>
-
-
     @endcan
-    {{-- RESIDENT --}}
-    @can('is-resident')
-        <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
 
+    @can('is-resident')
+        <x-sidebar-link href="#">
             My Visitors
         </x-sidebar-link>
 
@@ -52,8 +34,6 @@
             Complaints
 
         </x-sidebar-link>
-
-        </a>
     @endcan
 
 </div>
