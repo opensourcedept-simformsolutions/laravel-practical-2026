7ba7791b508fea9b4464b142ecd69c65f5eece8b
ankush dubey <ankush.dubey@simformsolutions.com>
2026-06-16 18:40:24 +0530

    feat:Added Crud feature for Flats with datatables

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <title>@yield('title', 'Dashboard')</title>
@@ -11,30 +10,37 @@
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.datatables.net/2.0.8/js/dataTables.min.js"></script>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
 </head>
 
 <body>
-    
-    @include('partials.navbar')
 
-    <div class="container-fluid">
-        <div class="row">
+@include('partials.navbar')
 
-            <div class="col-md-3 col-lg-2 bg-dark min-vh-100 p-0">
-                @include('partials.sidebar')
-            </div>
-    
-            <div class="col-md-9 col-lg-10 p-4">
-                @yield('content')
-            </div>
+<div class="container-fluid">
+    <div class="row">
+
+        <div class="col-md-3 col-lg-2 bg-dark min-vh-100 p-0">
+            @include('partials.sidebar')
         </div>
+
+        <div class="col-md-9 col-lg-10 p-4">
+            @yield('content')
+        </div>
+
     </div>
+</div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+@include('partials.footer')
 
-    @stack('scripts')
-    @yield('scripts')
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+@stack('scripts')
 
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
@@ -43,5 +49,4 @@
 <script src="https://cdn.datatables.net/1.13.8/js/dataTables.bootstrap5.min.js"></script>
 
 </body>
-
 </html>
