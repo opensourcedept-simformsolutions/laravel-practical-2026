2d200d6b983b941b4996470936fb58728ff61b29
ankush dubey <ankush.dubey@simformsolutions.com>
2026-06-17 12:47:10 +0530

    TE-T668 Added gate for authorization

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('is-admin', function ($user) {
+            return $user->role->name === 'admin';
+        });
+
+        Gate::define('is-gatekeeper', function ($user) {
+            return $user->role->name === 'gatekeeper';
+        });
+
+        Gate::define('is-resident', function ($user) {
+            return $user->role->name === 'resident';
+        });
     }
 }
