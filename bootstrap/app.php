040391ca522f3deda610fb4b465aa159da4f7e33
abhisimform <abhi.andani@simformsolutions.com>
2026-06-24 15:31:41 +0530

    Society and Passes Validation Updated with Global Trim String Middleware (#35)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\NormalizeInput;
 use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -8,14 +9,15 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withEvents(false)
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'role' => RoleMiddleware::class,
         ]);
+        $middleware->append(NormalizeInput::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
